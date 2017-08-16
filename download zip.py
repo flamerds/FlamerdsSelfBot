d55bcf9549c14a595b212c87fa1e664592a399fe
flamerds <flamerjds@gmail.com>
2017-08-16 16:30:38 -0400

    Rename download zip.py to update.py

--- download zip.py
+++
@@ -1,6 +0,0 @@
-import urllib.request
-from pathlib import Path
-
-
-homePath = str(Path.home())
-urllib.request.urlretrieve("https://codeload.github.com/flamerds/FlamerdsSelfBot/zip/master", homePath + "/Desktop/FlamerdsSelfBot.zip")
---
+++ update.py
@@ -0,0 +1,6 @@
+import urllib.request
+from pathlib import Path
+
+
+homePath = str(Path.home())
+urllib.request.urlretrieve("https://codeload.github.com/flamerds/FlamerdsSelfBot/zip/master", homePath + "/Desktop/FlamerdsSelfBot.zip")
